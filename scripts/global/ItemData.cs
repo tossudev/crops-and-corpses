99a1964cf66fc6d594cead2c22646d3dad3e7d3b
tossudev <tossudev@gmail.com>
2023-09-28 22:43:24 +0300

    Added Item class and test items

---
+++ scripts/global/ItemData.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class ItemData : Node {
+
+    [Export] public string ItemsDirectory = "res://assets/resources/items/";
+    public Godot.Collections.Array<Item> Items = new Godot.Collections.Array<Item>();
+
+    public override void _Ready() {
+        _LoadItemsFromPath();
+        GD.Print(Items);
+        // _SortItemsUsingID();
+        // GD.Print(Items);
+    }
+
+    private void _LoadItemsFromPath() {
+
+        using var dir = DirAccess.Open(ItemsDirectory);
+
+        // Open item directory
+        if (dir != null) {
+            dir.ListDirBegin();
+            string fileName = dir.GetNext();
+
+            // Add all items from directory to resource array
+            while (fileName != "") {
+                string filePath = ItemsDirectory + fileName;
+                var resource = (Item)GD.Load(filePath);
+                Items.Add(resource as Item);
+
+                fileName = dir.GetNext();
+            }
+        }
+    }
+
+
+    // private void _SortItemsUsingID() {
+    //     Item item = Items[0];
+    // }
+}

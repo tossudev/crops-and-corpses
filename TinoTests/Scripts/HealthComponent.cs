61a1c12c404e9e155e329f706973ac37490abd96
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2023-09-29 14:58:16 +0300

    feat: hitbox, health and attack components

---
+++ TinoTests/Scripts/HealthComponent.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public partial class HealthComponent : Node2D
+{
+	[Export]
+	private float _maxHealth = 100.0f;
+	private float _health;
+
+	public override void _Ready()
+	{
+		_health = _maxHealth;
+	}
+
+	public void TakeDamage(Attack attack)
+	{
+		_health -= attack.damage;
+
+		GD.Print("Health: " + _health);
+
+		if (_health <= 0)
+		{
+			GetParent().QueueFree();
+		}
+	}
+
+	public void Heal(Attack attack)
+	{
+		_health += attack.damage;
+
+		if (_health > _maxHealth)
+		{
+			_health = _maxHealth;
+		}
+	}
+}

d8d0e4e76c59eaaa117c248eae5824845fe95259
DanielH1337 <62145497+DanielH1337@users.noreply.github.com>
2023-09-30 14:54:58 +0300

    Starting SpawnScript

--- DaniTests/Scripts/SpawnScriptTest.cs
+++ DaniTests/Scripts/SpawnScriptTest.cs
@@ -1,15 +1,29 @@
 using Godot;
 using System;
+using System.Collections;
+using System.Linq;
 
 public partial class SpawnScriptTest : Node2D
 {
-
+	Node2D[] spawnPoints;
+	Timer spawnDelay;
 	PackedScene packedScene;
 	TimeManager dayTimeCheck;
 	bool isNightOrDay;
+	string isNightOrDayString;
+	private bool spawnCheck = true;
 
 	public override void _Ready()
 	{
+		spawnPoints = new Node2D[3];
+		spawnDelay =GetNode<Timer>("Timer");
+		
+		for(int i = 0; i < 3; i++)
+		{
+			spawnPoints[i] = GetNode<Node2D>("SpawnPoint"+i);
+			GD.Print(spawnPoints[i]);
+		}
+		
 		packedScene = (PackedScene)GD.Load("res://LilianTests/Prefabs/roaming_zombie.tscn");
 		dayTimeCheck = GetNode<TimeManager>("SunlightContainer");
 	}
@@ -24,7 +38,7 @@ public partial class SpawnScriptTest : Node2D
 				this.AddChild(prefab);
 				
 				isNightOrDay = dayTimeCheck.returnTimeOfDay(isNightOrDay);
-				string isNightOrDayString = isNightOrDay ? "DayTime" : "NighTime";
+				isNightOrDayString = isNightOrDay ? "DayTime" : "NighTime";
 				if(isNightOrDayString == "DayTime")
 				{
 					GD.Print("It is Day!");
@@ -37,4 +51,29 @@ public partial class SpawnScriptTest : Node2D
 			
 		}
     }
+	public override void _Process(double delta)
+	{
+		isNightOrDay = dayTimeCheck.returnTimeOfDay(isNightOrDay);
+		isNightOrDayString = isNightOrDay ? "DayTime" : "NighTime";
+		if(isNightOrDayString == "DayTime" && spawnCheck)
+		{
+			spawnCheck = false;
+			GD.Print("hei");
+			spawnDelay.Timeout += ZombieSpawn;
+
+		}
+	}
+	private void ZombieSpawn()
+	{
+		
+		for(int i = 0; i < 3; i++)
+		{
+			GD.Print("Hoi");
+			CharacterBody2D prefab = (CharacterBody2D)packedScene.Instantiate();
+			prefab.Position = spawnPoints[i].Position;
+			AddChild(prefab);
+		
+		}
+		spawnCheck = true;
+	}
 }

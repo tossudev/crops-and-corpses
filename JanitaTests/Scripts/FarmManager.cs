c0c10852a805b78c6c19d63c319dbd9ef1c0ff6f
kanitaa <janita.kor@gmail.com>
2023-10-02 14:18:19 +0300

    visual indicators to what is happening with plant

--- JanitaTests/Scripts/FarmManager.cs
+++ JanitaTests/Scripts/FarmManager.cs
@@ -10,12 +10,15 @@ public partial class FarmManager : Node
 
 	List<Plant> _plants = new List<Plant>();
 
+
+	bool _isWaterCanEquipped, _isBugSprayEquipped;
+
 	public override void _Ready()
 	{
 
 		if(instance==null)instance=this;else QueueFree();
 		InitializePlants();
-
+		_isWaterCanEquipped = _isBugSprayEquipped=true;
 
 	}
 
@@ -48,6 +51,20 @@ public partial class FarmManager : Node
 		}
 
 		return null;
+	}
+
+
+	public void EquipWaterCan(bool isEquipped){
+		_isWaterCanEquipped = isEquipped;
+	}
+	public bool IsWaterCanEquipped(){
+		return _isWaterCanEquipped;
+	}
+	public void EquipBugSpray(bool isEquipped){	
+		_isBugSprayEquipped = isEquipped;
+	}
+	public bool IsBugSprayEquipped(){
+		return _isBugSprayEquipped;
 	}
 	
 }

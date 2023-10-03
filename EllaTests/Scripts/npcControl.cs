687fa778e0ecb2554261cfc2c442fa3096baa2a6
ellaannika <ella.arokivi@metropolia.fi>
2023-10-03 19:04:50 +0300

    Npc movement

--- EllaTests/Scripts/npcControl.cs
+++ EllaTests/Scripts/npcControl.cs
@@ -16,23 +16,34 @@ public partial class npcControl : CharacterBody2D
 	}
 	public States CurrentState;
 	public NavigationAgent2D navigationAgent;
-	private List<Marker2D> waypointsFarm = new List<Marker2D>();
-	private int waypointIndex;
-	Vector2 targetPos;
-	Vector2 direction;
-	public float speed = 50;
-
-	public float timerStart;
-	public float timerInterval = 40;
+	public float speed = 90;
+	List<Marker2D> _waypointsFarm = new List<Marker2D>();
+	List<Marker2D> _waypointsTask = new List<Marker2D>();
+	List<Marker2D> _waypointTaskCompleted = new List<Marker2D>();
+	int _waypointIndex;
+	Vector2 _targetPos;
+	Vector2 _direction;
+	Timer _taskTimer;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		timerStart = 0;
+		// Setting up the timer
+		_taskTimer = new Timer
+		{
+			WaitTime = 20f,
+			OneShot = true
+		};
+		AddChild(_taskTimer);
+
 		navigationAgent = GetNode<NavigationAgent2D>("NavigationAgent2D");
-		waypointsFarm = GetTree().GetNodesInGroup("WaypointFarm").Select(saar => saar as Marker2D).ToList();
+
+		// Access to group
+		_waypointsFarm = GetTree().GetNodesInGroup("WaypointFarm").Select(saar => saar as Marker2D).ToList();
+		_waypointsTask = GetTree().GetNodesInGroup("Taskpoint").Select(saar => saar as Marker2D).ToList();
+		_waypointTaskCompleted = GetTree().GetNodesInGroup("TaskCompleted").Select(saar => saar as Marker2D).ToList();
 
 		CurrentState = States.Patrol;
-		navigationAgent.TargetPosition = waypointsFarm[0].GlobalPosition;
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -41,6 +52,19 @@ public partial class npcControl : CharacterBody2D
 		switch (CurrentState)
 		{
 			case States.Patrol:
+				speed = 90;
+
+				_direction = GlobalPosition.DirectionTo(_targetPos);
+				_targetPos = navigationAgent.TargetPosition;
+				Velocity = _direction * speed;
+				MoveAndSlide();
+
+				if (Input.IsActionJustPressed("Click"))
+				{
+					GD.Print("Task1 started");
+					CurrentState = States.Task;
+					_taskTimer.Start();
+				}
 
 				if (navigationAgent.IsNavigationFinished())
 				{
@@ -48,38 +72,71 @@ public partial class npcControl : CharacterBody2D
 					return;
 				}
 
-				if (Input.IsKeyPressed(Key.E))
-				{
-					CurrentState = States.Task;
-				}
-
-				GD.Print(waypointIndex);
-
-				direction = GlobalPosition.DirectionTo(targetPos);
-				targetPos = navigationAgent.TargetPosition;
-				Velocity = direction * speed;
-				MoveAndSlide();
-
 				break;
 
 			case States.Task:
-				GD.Print("Task1 started");
+
+				_direction = GlobalPosition.DirectionTo(_targetPos);
+				_targetPos = navigationAgent.TargetPosition;
+				Velocity = _direction * speed;
+				MoveAndSlide();
+
+				if (navigationAgent.IsNavigationFinished() && !_taskTimer.IsStopped())
+				{
+					MoveToWaypoints();
+					return;
+				}
+
+				if (_taskTimer.IsStopped())
+				{
+					TaskCompleted();
+					if (Input.IsActionJustPressed("Click"))
+					{
+						GD.Print("Task1 Completed");
+						CurrentState = States.Patrol;
+					}
+
+				}
 				break;
 
 			case States.Dialogue:
+				// Adding dialogue window where player can choose which task is being done
+				// After that goes to task state
 				break;
 		}
 	}
 
 	private void MoveToWaypoints()
 	{
-		waypointIndex += 1;
-		if (waypointIndex > waypointsFarm.Count - 1)
+		if (CurrentState == States.Patrol)
 		{
-			waypointIndex = 0;
+			GD.Print(_waypointIndex);
+			_waypointIndex += 1;
+			if (_waypointIndex > _waypointsFarm.Count - 1)
+			{
+				_waypointIndex = 0;
+			}
+			navigationAgent.TargetPosition = _waypointsFarm[_waypointIndex].GlobalPosition;
 		}
-		navigationAgent.TargetPosition = waypointsFarm[waypointIndex].GlobalPosition;
+
+		if (CurrentState == States.Task)
+		{
+			GD.Print(_waypointIndex);
+			_waypointIndex += 1;
+			if (_waypointIndex > _waypointsTask.Count - 1)
+			{
+				_waypointIndex = 0;
+			}
+			navigationAgent.TargetPosition = _waypointsTask[_waypointIndex].GlobalPosition;
+		}
 	}
 
-
+	private void TaskCompleted()
+	{
+		navigationAgent.TargetPosition = _waypointTaskCompleted[0].GlobalPosition;
+		if (GlobalPosition.DistanceTo(_waypointTaskCompleted[0].GlobalPosition) < 1.0)
+		{
+			speed = 0;
+		}
+	}
 }

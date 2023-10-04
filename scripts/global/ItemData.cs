25eea14a23550fafdfad1d08d7026112e70707aa
reima852 <94969829+reima852@users.noreply.github.com>
2023-10-05 02:11:50 +0300

    Imports

--- scripts/global/ItemData.cs
+++ scripts/global/ItemData.cs
@@ -5,7 +5,7 @@ using System.Linq;
 public partial class ItemData : Node {
 
     [Export] public string ItemsDirectory = "res://assets/resources/items/";
-    public static Godot.Collections.Array<Item> Items = new Godot.Collections.Array<Item>();
+    public static Godot.Collections.Array<Item> items = new Godot.Collections.Array<Item>();
 
     public override void _Ready() {
         _LoadItemsFromPath();
@@ -23,7 +23,7 @@ public partial class ItemData : Node {
             while (fileName != "") {
                 string filePath = ItemsDirectory + fileName;
                 var resource = (Item)GD.Load(filePath);
-                Items.Add(resource as Item);
+                items.Add(resource as Item);
 
                 fileName = dir.GetNext();
             }
@@ -32,6 +32,6 @@ public partial class ItemData : Node {
 
     public static Item GetItemById(int id)
     {
-        return Items.First(item => item.ID == id);
+        return items.First(item => item.ID == id);
     }
 }

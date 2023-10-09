1769f4b1cee19fdef0c1d71602f9e496eaf966b5
DanielH1337 <62145497+DanielH1337@users.noreply.github.com>
2023-10-09 20:56:04 +0300

    starting curing mechanics

--- LilianTests/Scripts/RoamingZombie.cs
+++ LilianTests/Scripts/RoamingZombie.cs
@@ -8,9 +8,15 @@ public partial class RoamingZombie : CharacterBody2D
 	private CharacterBody2D _player;
 	private Attack _attack;
 	private HealthComponent _healthComponent;
+	[Export] private NodePath _characterNodePath = null;
+	private CharacterBody2D _thisZombie;
+	PackedScene packedScene;
 
     public override void _Ready()
     {
+		packedScene = (PackedScene)GD.Load("res://LilianTests/Prefabs/zombie_with_hitbox.tscn");
+
+		_thisZombie = GetNodeOrNull<CharacterBody2D>(_characterNodePath);
 		//attack.damage = 10.0f;
         _sprite = GetNode<Sprite2D>("Sprite2D");
 		//_healthComponent = GetNode<HealthComponent>("HealthComponent");
@@ -44,7 +50,23 @@ public partial class RoamingZombie : CharacterBody2D
 
 			if (_hitbox != null)
 			{
-                _hitbox.ApplyAttack(_attack);
+				_hitbox.ApplyAttack(_attack);
+
+				switch(_attack.effect)
+				{
+					case EffectType.Cure:
+					
+						Transform2D zombiePos = _thisZombie.Transform;
+						_thisZombie.QueueFree();
+						CharacterBody2D spawnNPC = (CharacterBody2D)packedScene.Instantiate();
+						spawnNPC.Transform = zombiePos;
+						AddChild(spawnNPC);
+						break;
+					default:
+					break;
+
+				}
+               	 
 			}
 			else 
 			{

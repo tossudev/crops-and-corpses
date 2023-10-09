b95bed869398cd1a86d62ad7700e385598a0df4d
ellaannika <ella.arokivi@metropolia.fi>
2023-10-09 23:45:12 +0300

    NPC dialogue window

---
+++ EllaTests/Scripts/DialogueControl.cs
@@ -0,0 +1,91 @@
+using Godot;
+using System;
+
+public partial class DialogueControl : Control
+{
+	Button _buttonOpt1;
+	Button _buttonOpt2;
+	[Export]
+	npcControl NPC;
+
+	RichTextLabel _nameText;
+	RichTextLabel _text;
+	ColorRect _backgroundColor;
+	public bool farmingTaskStarted = false;
+	public bool exitDialogue = false;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_backgroundColor = GetNode<ColorRect>("ColorRect");
+
+		_buttonOpt1 = GetNode<Button>("ColorRect/Button1");
+		_buttonOpt2 = GetNode<Button>("ColorRect/Button2");
+
+		_nameText = GetNode<RichTextLabel>("ColorRect/Name");
+		_text = GetNode<RichTextLabel>("ColorRect/Text");
+
+		_nameText.Text = "Name";
+		_text.Text = "Hello, do you need help?";
+		_buttonOpt1.Text = "Yes, I need help";
+		_buttonOpt2.Text = "Nevermind";
+
+		Visible = false;
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (NPC.dialogueWindow == true)
+		{
+			DialogueWindowVisible();
+		}
+	}
+
+	public void _on_button_1_button_up()
+	{
+		farmingTaskStarted = true;
+		Visible = false;
+	}
+
+	public void _on_button_2_button_up()
+	{
+		exitDialogue = true;
+		Visible = false;
+	}
+
+	private void DialogueWindowVisible()
+	{
+		GD.Print("Dialogue window opened");
+		Visible = true;
+		NPC.dialogueWindow = false;
+
+		if (NPC.CurrentState == npcControl.States.Patrol)
+		{
+			_text.Text = "Hello, do you need help?";
+			_buttonOpt1.Text = "Yes, I need help";
+			_buttonOpt2.Text = "Nevermind";
+
+			_buttonOpt1.Visible = true;
+		}
+
+		if (NPC.CurrentState == npcControl.States.Task)
+		{
+			_text.Text = "I'm busy";
+			_buttonOpt2.Text = "Nevermind";
+
+			_buttonOpt1.Visible = false;
+		}
+
+		if (NPC.CurrentState == npcControl.States.TaskCompleted)
+		{
+			_text.Text = "I did my job, do you need help with something else?";
+			_buttonOpt1.Text = "Yes, I need help";
+			_buttonOpt2.Text = "Nevermind";
+
+			_buttonOpt1.Visible = true;
+		}
+	}
+
+
+}
--- EllaTests/TestScene.cs
+++ EllaTests/TestScene.cs
@@ -3,12 +3,14 @@ using System;
 
 public partial class TestScene : Node2D
 {
+	npcControl NPC;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		npcControl NPC = (npcControl)GD.Load<PackedScene>("res://EllaTests/npc.tscn").Instantiate();
-        NPC.Position = new Vector2(500, 500);
-        AddChild(NPC);
+		/* NPC = (npcControl)GD.Load<PackedScene>("res://EllaTests/npc.tscn").Instantiate();
+		NPC.Position = new Vector2(500, 500);
+		AddChild(NPC); */
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.

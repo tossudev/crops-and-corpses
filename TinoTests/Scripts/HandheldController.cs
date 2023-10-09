5fd8230f4407640becd8cb896b87bc937505736d
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2023-10-09 15:38:41 +0300

    feat: axe tool, combat changes

--- TinoTests/Scripts/HandheldController.cs
+++ TinoTests/Scripts/HandheldController.cs
@@ -2,7 +2,7 @@ using Godot;
 using System;
 using System.Collections;
 
-public partial class WeaponController : Node2D
+public partial class HandheldController : Node2D
 {
     [Export] private Resource _weapon;
     [Export] private Resource _projectile;
@@ -17,24 +17,33 @@ public partial class WeaponController : Node2D
     private float _cooldown;
     private EffectType _effect;
     private float _reach;
+    private bool _holdAction;
     private bool _ranged;
     private float _speed;
     private float _drawTime;
 
-    private bool _isDrawing = false;
+    private bool _isDrawing;
 
     public override void _Ready()
+    {
+        Init(); //temp
+    }
+
+    private void Init()
     {
         _damage = (float)_weapon.Get("damage");
         _knockback = (float)_weapon.Get("knockback");
         _cooldown = (float)_weapon.Get("cooldown");
         _effect = (EffectType)(int)_weapon.Get("effect");
         _reach = (float)_weapon.Get("reach");
+        _holdAction = (bool)_weapon.Get("holdAction");
         _speed = (float)_weapon.Get("speed");
         _ranged = (bool)_weapon.Get("ranged");
         _drawTime = (float)_weapon.Get("drawTime");
 
         this.Scale = Vector2.One * _reach;
+
+        _isDrawing = false;
     }
 
     public override void _PhysicsProcess(double delta)
@@ -49,11 +58,13 @@ public partial class WeaponController : Node2D
     {
         _weapon = weapon;
         _projectile = projectile;
+
+        Init();
     }
 
     public void Use(bool canMelee)
     {
-        if (_timer.TimeLeft > 0 || _weapon == null)
+        if (_timer.TimeLeft > 0 || _isDrawing)
             return;
 
         _attack = new Attack
@@ -63,11 +74,11 @@ public partial class WeaponController : Node2D
             effect = _effect
         };
 
-        if (_ranged && !_isDrawing)
+        if (_ranged)
         {
             StartDraw();
         }
-        else if (canMelee)
+        else if (canMelee || _holdAction)
         {
             UseMelee();
         }
@@ -106,6 +117,14 @@ public partial class WeaponController : Node2D
         _animationPlayer.SpeedScale = 1 / _drawTime;
         _animationPlayer.Play("draw");
         _timer.Start(_drawTime);
+    }
+
+    public void Release()
+    {
+        if (_ranged)
+        {
+            ReleaseDraw();
+        }
     }
 
     public void ReleaseDraw()

c81ee1cd396927e48a52db8848d223b4a5bf913e
tossudev <tossudev@gmail.com>
2023-10-10 12:46:48 +0300

    Added sound system
    Added Audio-scene to autoload

---
+++ scripts/global/AudioController.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public partial class AudioController : Node {
+
+    AudioStreamPlayer sfxPlayer;
+    AudioStreamPlayer musicPlayer;
+    AudioStreamPlayer ambiencePlayer;
+
+    const string soundDirectory = "res://assets/Sounds/";
+
+
+    public override void _Ready() {
+        sfxPlayer = GetNode<AudioStreamPlayer>("GeneralSFX");
+        musicPlayer = GetNode<AudioStreamPlayer>("Music");
+        ambiencePlayer = GetNode<AudioStreamPlayer>("Ambience");
+
+        // PlayMusic("music/music_day.wav");
+        // PlayAmbience("ambiences/ambience_day.wav");
+    }
+
+
+    public void PlayMusic(string soundFile) {
+        AudioStream audio = GetAudioFromFile(soundFile);
+
+        musicPlayer.Stream = audio;
+        musicPlayer.Play();
+    }
+
+    public void PlayAmbience(string soundFile) {
+        AudioStream audio = GetAudioFromFile(soundFile);
+
+        ambiencePlayer.Stream = audio;
+        ambiencePlayer.Play();
+    }
+
+
+    AudioStream GetAudioFromFile(string soundFile) {
+        string fullPath = soundDirectory + soundFile;
+        AudioStream audio = (AudioStream)GD.Load(fullPath);
+
+        return audio;
+    }
+}

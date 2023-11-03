183768239079d6cfe846fad77a3c741a8b1855a2
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2023-11-03 10:21:46 +0200

    feat: Crafting Requirements, combat refactors

--- TinoTests/Scripts/Weapons/Weapon.cs
+++ TinoTests/Scripts/Weapons/Weapon.cs
@@ -8,10 +8,10 @@ public partial class Weapon : Resource
     [ExportCategory("Global")]
     [Export] public Item item { get; set; }
     [Export] public float damage { get; set; }
-    [Export] public Array<float> targetDamage { get; set; } = new Array<float> { 0, 0, 0 };
     [Export] public float knockback { get; set; }
     [Export] public float cooldown { get; set; }
     [Export] public EffectType effect { get; set; }
+    [Export] public TargetType targetType { get; set; }
 
     [ExportCategory("Melee")]
     [Export] public float reach { get; set; } = 1;

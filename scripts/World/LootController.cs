0dd05085f32584118d5e95a37736fd5005b0ad87
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2023-11-07 12:12:43 +0200

    feat: lootcontroller tweaks

--- scripts/World/LootController.cs
+++ scripts/World/LootController.cs
@@ -3,43 +3,54 @@ using System;
 using Godot.Collections;
 using System.Collections.Generic;
 
-public partial class GatheringObjectController : StaticBody2D
+public partial class LootController : StaticBody2D
 {
-	[Export] private GatheringObject _gatheringObject;
+	[Export] private Loot _loot;
 	[Export] private Sprite2D _sprite;
 
-	private int _maxDrop;
-	private List<Item> _items;
-	private float healthPercent;
+	private List<Item> _items = new List<Item>();
 
 	public override void _Ready()
 	{
-		_maxDrop = _gatheringObject.maxDrop;
-		_items = new List<Item>(_gatheringObject.items);
+		if (_loot == null)
+		{
+			GD.PrintErr("LootController: Loot is null");
+			return;
+		}
+
+		foreach (var item in _loot.lootItems)
+		{
+			for (int i = 0; i < item.quantity; i++)
+			{
+				_items.Add(item.item);
+			}
+		}
 	}
 
 	private void OnHealth(float health)
 	{
-		DropItems();
+		if (_items.Count > 1)
+			DropItems();
 
 		if (health <= 0)
 		{
+			DropItems(_items.Count);
 			QueueFree();
 		}
 	}
 
-	private void DropItems()
+	private void DropItems(int dropAmount = 1)
 	{
-		int weight = 3;
-		int dropAmount = GD.RandRange(0, weight) == 0 ? GD.RandRange(1, _maxDrop) : 1;
-
 		for (int i = 0; i < dropAmount; i++)
 		{
+			GD.Print(_items[_items.Count - 1].Name);
 			int randIndex = (int)GD.RandRange(0, _items.Count - 1);
 
 			RawInventoryItem dropItem = new RawInventoryItem(_items[randIndex].ID, _items[randIndex].Name, 1, _items[randIndex].StackSize);
 
 			Node2D droppedItem = PlayerInventoryController.CreateDroppedItem(dropItem, GlobalPosition, GetParent());
+
+			_items.RemoveAt(randIndex);
 
 			MoveDropItem(droppedItem);
 		}

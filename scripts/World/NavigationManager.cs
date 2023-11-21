cacbc98d7f52391d054b1c4456bde53a07ecb1c5
tossudev <tossudev@gmail.com>
2023-11-21 13:51:08 +0200

    Automatic navigation working
    Added a bunch of polygons to obstacles

--- scripts/World/NavigationManager.cs
+++ scripts/World/NavigationManager.cs
@@ -9,7 +9,7 @@ public partial class NavigationManager : Node2D {
 	NavigationRegion2D _regionNode;
 	NavigationPolygon _regionPolygon;
 
-	Godot.Collections.Array<Vector2[]> _obstacleAreas;
+	Godot.Collections.Array<Vector2[]> _obstacleAreas = new ();
 	[Export] public Vector2[] arrayTest;
 
 
@@ -22,26 +22,12 @@ public partial class NavigationManager : Node2D {
 
 
 	void InitRegion() {
-
 		// Get all obstacles
 		foreach (Polygon2D _obstacleArea in GetTree().GetNodesInGroup(_obstacleGroupName)) {
-			Vector2[] _polygonPoints = GetPolygonFromObject(_obstacleArea);
-			// GD.Print(_polygonPoints[0]);
-			_obstacleAreas.Append(_polygonPoints);
+			Vector2[] polygonPoints = GetPolygonFromObject(_obstacleArea);
+			_obstacleAreas.Add(polygonPoints);
+			_regionPolygon.AddOutline(polygonPoints);
 		}
-
-		// Carve out every obstacle from the navigation region
-		foreach (Vector2[] _area in _obstacleAreas) {
-			Vector2[] _polygonToRemove = Array.Empty<Vector2>();
-
-			foreach (Vector2 _pos in _area) {
-				_polygonToRemove.Append(_pos);
-			}
-
-			_regionPolygon.AddOutline(_polygonToRemove);
-		}
-
-		GD.Print(_obstacleAreas);
 
 		// Update navigation region
 		_regionPolygon.MakePolygonsFromOutlines();
@@ -49,16 +35,7 @@ public partial class NavigationManager : Node2D {
 
 
 	static Vector2[] GetPolygonFromObject(Polygon2D _obstacleArea) {
-		// TODO:
-		// Figure out why the hell a Vector2[] is always empty when trying to fetch it
-
-		// var test = _obstacleArea.Get("polygon");
-		// Vector2[] _polygonPoints = _obstacleArea.Get("polygon").Obj as Vector2[];
-
 		Vector2[] _polygonPoints = (Vector2[]) _obstacleArea.Get("polygon");
-
-		GD.Print(_polygonPoints[0]);	// Prints "(616, -112)"
-		GD.Print(_polygonPoints);		// Prints "Godot.Vector2[]"
 
 		// Adjust areas local position to global
 		int _posIndex = 0;

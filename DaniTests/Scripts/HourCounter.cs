84c0e98c26983a3c5520bb228acbcfa7a4e549ec
DanielH1337 <62145497+DanielH1337@users.noreply.github.com>
2023-11-22 17:45:13 +0200

    adding zombieskeleton rig for zombie with hitbox prefab

--- DaniTests/Scripts/HourCounter.cs
+++ DaniTests/Scripts/HourCounter.cs
@@ -11,7 +11,7 @@ public partial class HourCounter : Label
 	
 
 
-	private float hourOfDay=6;
+	private float hourOfDay;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{

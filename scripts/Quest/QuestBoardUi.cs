0bd5546ac475daae0e87d370a1235ed05fefa529
reima852 <94969829+reima852@users.noreply.github.com>
2023-11-27 13:29:19 +0200

    Fixed project maybe

--- scripts/Quest/QuestBoardUi.cs
+++ scripts/Quest/QuestBoardUi.cs
@@ -4,29 +4,29 @@ using System;
 public partial class QuestBoardUi : Control
 {
 
-const string MenuButtonPath = "DiffcultyButton";
+const string MenuButtonPath = "%DiffcultyButton";
 	MenuButton QuestButton;
 
-	const string button_forrest =  "ForrestButton";
-	Button ForrestButton;
+	const string button_forest =  "%ForestButton";
+	Button ForestButton;
 
-	const string button_ruins = "RuinsButton";
+	const string button_ruins = "%RuinsButton";
 	Button RuinsButton;
 
-	const string button_Cave = "CaveButton";
+	const string button_Cave = "%CaveButton";
 	Button CaveButton;
 
 
-	const string button_Dif1 = "dif1";
+	const string button_Dif1 = "%dif1";
 	Button Dif1Button;
 
-	const string button_Dif2 = "dif2";
+	const string button_Dif2 = "%dif2";
 	Button Dif2Button;
 	
-	const string button_Dif3 = "dif3";
+	const string button_Dif3 = "%dif3";
 	Button Dif3Button;
 
-	const string label_CDiff = "Cdif";
+	const string label_CDiff = "%Cdif";
 	Label CDiffLabel;
 
 	QuestManager questManager;
@@ -36,58 +36,34 @@ const string MenuButtonPath = "DiffcultyButton";
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		ForrestButton = GetNode<Button>(button_forrest);
+		ForestButton = GetNode<Button>(button_forest);
 		RuinsButton = GetNode<Button>(button_ruins);
 		CaveButton = GetNode<Button>(button_Cave);
 		Dif1Button = GetNode<Button>(button_Dif1);
 		Dif2Button = GetNode<Button>(button_Dif2);
 		Dif3Button = GetNode<Button>(button_Dif3);
 		CDiffLabel = GetNode<Label>(label_CDiff);
+		
+		
+		// Button mapping
+		ForestButton.Pressed += () => questManager.StartForestQuest();
+		RuinsButton.Pressed += () => questManager.StartRuinsQuest();
+		CaveButton.Pressed += () => questManager.StartCaveQuest();
+
+		Dif1Button.Pressed += () => SetQuestDifficulty(1);
+		Dif2Button.Pressed += () => SetQuestDifficulty(2);
+		Dif3Button.Pressed += () => SetQuestDifficulty(3);
 	}
 
 	public void _Process(double delta)
 	{
 		CDiffLabel.Text = questManager.SelectedDifficulty.ToString();
 	}
+    
 
-
-
-	
-
-	public void On_forrest_button_pressed()
+	void SetQuestDifficulty(int diff)
 	{
-		questManager.StartForrestQuest();
-		GD.Print("Forrest button pressed");
-	}
-	
-	
-	public void On_RuinsButton_pressed()
-	{
-		questManager.StartRuinsQuest();
-		GD.Print("Ruins button pressed");
-	}
-
-	public void On_CaveButton_pressed()
-	{
-		questManager.StartCaveQuest();
-		GD.Print("Cave button pressed");
-	}
-
-	public void On_dif1_pressed()
-	{
-		questManager.SelectedDifficulty = 1;
-		GD.Print("Difficulty 1 button pressed");
-	}
-
-	public void On_dif2_pressed()
-	{	questManager.SelectedDifficulty = 2;
-		GD.Print("Difficulty 2 button pressed");
-	}
-
-	public void On_dif3_pressed()
-	{	
-		questManager.SelectedDifficulty = 3;
-		GD.Print("Difficulty 3 button pressed");
+		questManager.SelectedDifficulty = diff;
 	}
 
 	public void addRawVillagers()

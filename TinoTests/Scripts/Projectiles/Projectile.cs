110e14fd92e9e1b4b7e7ea7fec12b7a73991775e
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2023-11-28 13:34:16 +0200

    feat: cave updates, player and projectile shadows

--- TinoTests/Scripts/Projectiles/Projectile.cs
+++ TinoTests/Scripts/Projectiles/Projectile.cs
@@ -8,5 +8,6 @@ public partial class Projectile : Resource
     [Export] public float airtime { get; set; } = 2;
     [Export] public float despawnTime { get; set; } = 0.5f;
     [Export] public bool objectCollision { get; set; } = false;
+    [Export] public float height { get; set; } = 0.5f;
     [Export] public EffectType effect { get; set; } = 0;
 }

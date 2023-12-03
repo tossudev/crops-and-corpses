3f54b592965722a05185110f62542a668ed31160
Lilian Aasa <91128698+AALilian@users.noreply.github.com>
2023-12-03 22:39:42 +0200

    bridge and forest stuff

---
+++ scripts/World/BridgeQuest.cs
@@ -0,0 +1,61 @@
+using Godot;
+using System;
+
+public partial class BridgeQuest : Node2D
+{
+	[Export] private Node2D _bridge;
+	private CanvasLayer _canvasLayer;
+	private bool _playerInArea;
+	private bool _questFinished;
+	private const int WOOD_NEEDED = 10;
+
+	public override void _Ready()
+	{
+		_canvasLayer = GetNode<CanvasLayer>("%BridgeQuestUI");
+	}
+
+	private void OnInputEvent(Node viewport, InputEvent @event, long shapeIdx)
+	{
+		if(@event is InputEventMouseButton button && _playerInArea)
+		{
+			_canvasLayer.Visible = true;
+		}
+	}
+	private void OnBodyEntered(Node2D body)
+	{
+		if (body.IsInGroup("player"))
+		{
+			_playerInArea = true;
+
+			if (PlayerInventoryData.ExistsInInventory(1, WOOD_NEEDED))
+			{
+				GetNode<Button>("%FinishQuestBtn").Disabled = false;	
+				_questFinished = true;			
+			}
+		}
+	}
+
+	private void OnBodyExited(Node2D body)
+	{
+		if (body.IsInGroup("player"))
+		{
+			_playerInArea = false;
+		}
+	}
+
+	private void OnCloseButtonPressed()
+	{
+		_canvasLayer.Visible = false;
+	}
+
+	private void OnFinishQuestButtonPressed()
+	{
+		if (_questFinished)
+		{
+			if (_bridge != null) _bridge.Visible = true;
+			GetNode<StaticBody2D>("%Barrier").QueueFree();
+			_canvasLayer.Visible = false;
+			GetNode<Label>("%BridgeLabel").Text = "Nice job, you finished the bridge! I wonder where it leads to...";
+		}		
+	}
+}

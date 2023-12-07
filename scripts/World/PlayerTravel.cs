52b8e81e70648935b244cb90f2d0f63066918ded
otso salmenperä <72009558+Salmot@users.noreply.github.com>
2023-12-07 16:54:30 +0200

    hide ruins sign

--- scripts/World/PlayerTravel.cs
+++ scripts/World/PlayerTravel.cs
@@ -3,6 +3,23 @@ using System;
 
 public partial class PlayerTravel : Node2D
 {
+	const string RUINS_STREET_SIGN_GROUP = "%ruins_sign";
+	Area2D forrestSign;
+
+	public override void _Ready()
+	{
+		base._Ready();
+		forrestSign = GetNode<Area2D>(RUINS_STREET_SIGN_GROUP);
+		if( TownManager.currentTownStats.isRuinsUnlocked)
+		{
+			forrestSign.Visible = true;
+		}else
+		{
+			forrestSign.Visible = false;
+		}
+	}
+
+
 	void TravelTown(Node viewport, InputEvent @event, long shapeIdx)
 	{
 		if (@event is InputEventMouseButton button && button.IsPressed() && button.ButtonIndex == MouseButton.Left)
@@ -20,7 +37,7 @@ public partial class PlayerTravel : Node2D
 
 	void TravelRuins(Node viewport, InputEvent @event, long shapeIdx)
 	{
-		if (@event is InputEventMouseButton button && button.IsPressed() && button.ButtonIndex == MouseButton.Left)
+		if (@event is InputEventMouseButton button && button.IsPressed() && button.ButtonIndex == MouseButton.Left && TownManager.currentTownStats.isRuinsUnlocked)
 		{
 			SceneManager.ChangeScene(this, Scene.Ruins);
 		}
